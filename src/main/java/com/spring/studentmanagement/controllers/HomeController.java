ed81a320e12fd0f70bbb27168c6868dc8db4c515
Gabriel Ion <gabriel.ion2005@gmail.com>
2026-01-05 23:57:58 +0200

    Created thymeleaf views, controllers

---
+++ src/main/java/com/spring/studentmanagement/controllers/HomeController.java
@@ -0,0 +1,39 @@
+package com.spring.studentmanagement.controllers;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/home")
+public class HomeController {
+
+    @Value("${db.name}")
+    private String databaseName;
+
+    @Value("${db.password}")
+    private String databaseUser;
+
+    @Value("${server.port}")
+    private int serverPort;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    @Value("${db.user}")
+    private String user;
+
+    @GetMapping
+    public String getHomeView(Model model){
+
+        model.addAttribute("dbName", this.databaseName);
+        model.addAttribute("dbUsername", this.contextPath);
+        model.addAttribute("serverPort", this.serverPort);
+        model.addAttribute("contextPath", this.contextPath);
+        model.addAttribute("user", this.user);
+        return "home";
+    }
+}
---
+++ src/main/java/com/spring/studentmanagement/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.spring.studentmanagement.repositories;
+
+import com.spring.studentmanagement.models.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<AppUser, Long> {
+}
