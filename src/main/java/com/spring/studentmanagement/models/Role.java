92d84cd9bc1c31d172019cb98f45a8e08541aa60
Gabriel Ion <gabriel.ion2005@gmail.com>
2026-01-05 23:59:24 +0200

    Created AppUser, Role entities and UserRepository

---
+++ src/main/java/com/spring/studentmanagement/models/Role.java
@@ -0,0 +1,25 @@
+package com.spring.studentmanagement.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "role", schema = "public")
+@Entity(name = "Role")
+public class Role {
+
+    @Id
+    @Column(name = "roleId")
+    private long roleId;
+
+    @Column(name = "role_name", length = 45, unique = true ,nullable = false)
+    private String roleName;
+
+}
